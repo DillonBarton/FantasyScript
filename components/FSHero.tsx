e53f73ac9e8dd1be112aea25e7246ba98edfdd59
Dillon <dillon_barton@hotmail.com>
2021-08-15 12:09:16 +0100

    new laptop

--- components/FSHero.tsx
+++ components/FSHero.tsx
@@ -1,3 +1,6 @@
+import Particles from 'react-particles-js'
+import particleParams from '../particle.config'
+
 import styles from '../styles/fsHero.module.css'
 
 import{ FaArrowAltCircleRight } from 'react-icons/fa'
@@ -7,6 +10,7 @@ export default function Hero(){
 
     return(
         <section className={styles.heroSection}>
+            <Particles params={particleParams} className={`${styles.particles} boxW100H100`} />
 
             <div className={`${styles.heroText} flexColumn ss`}>
 
@@ -39,8 +43,8 @@ export default function Hero(){
                     <path fill="url(#MyGradient)" x="0" y="0" d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className={styles.shapeFillLayer1}></path>
                     <defs>
                         <linearGradient id="MyGradient" gradientTransform="rotate(90)">
-                            <stop offset="5%" stopColor="rgba(70, 131, 180, 0.6)" />
-                            <stop offset="95%" stopColor="rgba(0, 0, 0, 0.05)" />
+                            <stop offset="5%" stopColor="rgba(0, 0, 0, 0.2)" />
+                            <stop offset="95%" stopColor="rgba(0, 0, 0, 0.6)" />
                         </linearGradient>
                     </defs>
                 </svg>
@@ -50,8 +54,8 @@ export default function Hero(){
                 <svg data-name="Layer 3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                     <path fill="url(#MyGradient2)" d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className={styles.shapeFillLayer2}></path>
                     <linearGradient id="MyGradient2" gradientTransform="rotate(90)">
-                        <stop offset="5%" stopColor="rgba(70, 131, 180, 0.4)" />
-                        <stop offset="95%" stopColor="rgba(0, 0, 0, 0.05)" />
+                        <stop offset="5%" stopColor="rgba(0, 0, 0, 0.25)" />
+                        <stop offset="95%" stopColor="rgba(0, 0, 0, 0.4)" />
                     </linearGradient>
                 </svg>
             </div>
@@ -59,8 +63,8 @@ export default function Hero(){
                 <svg data-name="Layer 3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
                     <path fill="url(#MyGradient3)" d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className={styles.shapeFillLayer3}></path>
                     <linearGradient id="MyGradient3" gradientTransform="rotate(90)">
-                        <stop offset="5%" stopColor="rgba(70, 131, 180, 0.4)" />
-                        <stop offset="95%" stopColor="rgba(0, 0, 0, 0.05)" />
+                        <stop offset="5%" stopColor="rgba(0, 0, 0, 0.25)" />
+                        <stop offset="95%" stopColor="rgba(0, 0, 0, 0.4)" />
                     </linearGradient>
                 </svg>
             </div>
