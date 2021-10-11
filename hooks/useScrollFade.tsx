5ccb0a9227961833aacfb65150a0dda8708cf728
DillonBarton <dillon_barton@hotmail.com>
2021-10-11 10:27:44 +0100

    removal of console logs

--- hooks/useScrollFade.tsx
+++ hooks/useScrollFade.tsx
@@ -7,7 +7,6 @@ export default function useScrollFade(ref, animation, setAnimation){
         window.addEventListener("scroll", ()=>{
             let offset = document.documentElement.clientHeight + window.scrollY;
             let scrollHeight = ref.current.getBoundingClientRect().top + window.scrollY;
-            console.log(scrollHeight, offset);
             if( offset >= scrollHeight && animation === false ) {
                 setAnimation(true)
             } else if( offset <= scrollHeight && animation === true ) {
